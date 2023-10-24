7b011f89193bd26796a349ef619eb70dc3b5e8e4
ra.luca <costin.iraluca89@gmail.com>
2023-10-24 17:17:53 +0200

    nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "@/views/EventList.vue";
-import EventDetails from "@/views/EventDetails.vue";
+import EventLayout from "@/views/event/Layout.vue";
+import EventDetails from "@/views/event/Details.vue";
+import EventRegister from "@/views/event/Register.vue";
+import EventEdit from "@/views/event/Edit.vue";
 import About from "@/views/About.vue";
 
 const routes = [
@@ -12,9 +15,27 @@ const routes = [
   },
   {
     path: "/event/:id",
-    name: "EventDetails",
+    name: "EventLayout",
     props: true,
-    component: EventDetails,
+    component: EventLayout,
+    children: [ // this is an array, also this is a child-class to the parent class.
+      {
+        path: "", // the path will be the one of the parent: event/:id path.
+        name: "EventDetails",
+        //you don't need props here cuz it will be enherited.
+        component: EventDetails,
+      },
+      {
+        path: "register",
+        name: "EventRegister",
+        component: EventRegister,
+      },
+      {
+        path: "edit",
+        name: "EventEdit",
+        component: EventEdit,
+      },
+    ]
   },
   {
     path: "/about",
---
+++ src/views/event/Layout.vue
@@ -0,0 +1,35 @@
+<script setup>
+import EventService from "@/services/EventService.js";
+import { computed, onMounted, ref } from "vue";
+
+const props = defineProps(["id"]);
+
+const event = ref("");
+const id = computed(() => props.id);
+onMounted(() => {
+  EventService.getEvent(id.value)
+      .then((response) => {
+        event.value = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+});
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <div id="nav">
+
+      <router-link :to="{name: 'EventDetails'}">Details</router-link>
+      |  <!--url will look something like: /event/2 -->
+      <router-link :to="{name: 'EventRegister'}">Register</router-link>
+      |  <!--url will look something like: /event/2/register -->
+      <router-link :to="{name: 'EventEdit'}">Edit</router-link>
+      <!--url will look something like: /event/2/edit -->
+
+    </div>
+    <router-view :event="event" />
+  </div>
+</template>
